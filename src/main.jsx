91353aa41ac175bc9fea56dfb6df6e0ce0c1b998
Ignasi CP <116747620+icplana@users.noreply.github.com>
2023-07-05 17:34:42 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './styles.css'
+import { JournalApp } from './JournalApp'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './store'
+import { Provider } from 'react-redux'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store = { store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
